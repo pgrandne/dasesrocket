da5de75b799128f3afe09cf2ed54723290e2238c
pgrandne <perrin.grandne@protonmail.com>
2022-05-05 17:19:30 +0200

    responsive for title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import { Alert, Card, Container, Nav, Navbar } from 'react-bootstrap'
+import { Alert, Card, Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
 import { useState } from 'react';
 
 const App = () => {
   const [show, setShow] = useState(true);
+
+  const handleCard = () => {
+    console.log('ok')
+  }
 
 
   return (
@@ -22,36 +25,50 @@ const App = () => {
         </Container>
       </Navbar>
       <Container>
-        <h1>Acteurs de l'écosystème de l'éducation et des compétences</h1>
-        <Card
-          bg="secondary"
-          key="secondary"
-          text="white"
-          style={{ width: '18rem' }}
-          className="mb-2"
-        >
-          <Card.Body>
-            <Card.Title>Partagez un besoin</Card.Title>
-            <Card.Text>
-              Besoin de données ? De services ? D'outils pour interagir avec des données ?
-            </Card.Text>
-          </Card.Body>
-        </Card>
-
-        <Card
-          bg="dark"
-          key="dark"
-          text="white"
-          style={{ width: '18rem' }}
-          className="mb-2"
-        >
-          <Card.Body>
-            <Card.Title>Parcourir les opportunités</Card.Title>
-            <Card.Text>
-              Des acteurs partagent déjà des services, données et outils, consultez-les
-            </Card.Text>
-          </Card.Body>
-        </Card>
+        <Row>
+          <Col md={2}/>
+          <Col md={8}>
+            <h1>Acteurs de l'écosystème de l'éducation et des compétences</h1>
+          </Col>
+          <Col md={2}/>
+        </Row>
+        <Row>
+          <Col />
+          <Col sm>
+            <Card
+              bg="secondary"
+              key="secondary"
+              text="white"
+              className="mb-2"
+              onClick={handleCard}
+            >
+              <Card.Body>
+                <Card.Title>Partagez un besoin</Card.Title>
+                <Card.Text>
+                  Besoin de données ? De services ? D'outils pour interagir avec des données ?
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm>
+            <Card
+              bg="dark"
+              key="dark"
+              text="white"
+              // style={{ width: '18rem' }}
+              className="mb-2"
+              onClick={handleCard}
+            >
+              <Card.Body>
+                <Card.Title>Parcourir les opportunités</Card.Title>
+                <Card.Text>
+                  Des acteurs partagent déjà des services, données et outils, consultez-les
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm />
+        </Row>
       </Container>
 
       {show &&
